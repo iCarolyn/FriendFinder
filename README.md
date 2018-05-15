02f49047e2eb4c01da29e3b39316148c3574674c
Carolyn Rojsutivat <carolynrojsutivat@Carolyns-MacBook-Pro.local>
2018-05-15 13:01:51 -0500

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# FriendFinder
-Friend Finder
+# BFFL Finder - Friend Finder App
+An app created using Node.js, Express, Body Parser, and Path to find a friend that you're most compatible with.
+
+## Screenshots
+
+Home page
+
+![](screencap1.png)
+
+The Survey
+
+![](screencap2.png)
+
+Result
+
+![](screencap3.png)
+
+## Tech/framework used
+
+<b>Built with</b>
+- [Node.js](https://nodejs.org/en/)
+- [Express](https://expressjs.com/)
+- [Body Parser](https://github.com/expressjs/body-parser)
+- [Path](https://www.npmjs.com/package/path)
+
+## License
+
+Northwestern University © [Carolyn Rojsutivat]()
---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Thor',
+		photo: 'https://static0.srcdn.com/wordpress/wp-content/uploads/2017/02/Thor-The-Dark-World-Convergence.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Iron Man',
+		photo: 'https://geekandsundry.com/wp-content/uploads/2016/08/ironman-cover.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Spider Man',
+		photo: 'http://static.businessinsider.com/image/594bd7e4d084cc78008b4908-750.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Doctor Strange',
+		photo: 'https://cdn.pastemagazine.com/www/articles/dr%20strange%20review%20main%20%28Custom%29.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Incredible Hulk',
+		photo: 'https://www.themarysue.com/wp-content/uploads/2018/04/Best-Movie-Reboots-The-Incredible-Hulk.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Black Widow',
+		photo: 'http://cdn.darkhorizons.com/wp-content/uploads/2018/01/jac-schaeffer-to-pen-marvels-black-widow.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Star Lord',
+		photo: 'http://marvel-avengers.com/wp-content/uploads/2017/05/Starlord-Peter-Quill-758x496.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Groot',
+		photo: 'https://media1.popsugar-assets.com/files/thumbor/BpRj-io9vKkVi7OO1M-e3kS3qdo/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2018/05/10/643/n/24155406/tmp_SRmpPl_989a13a60345cd19_MCDGUOF_EC248.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Captain Marvel',
+		photo: 'http://cdn.wegotthiscovered.com/wp-content/uploads/2018/01/Captain-Marvel-hires-Tomb-Raider-reboot-scribe-to-pen-the-latest-draft-of-script-1.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Black Panther',
+		photo: 'https://www.counterpunch.org/wp-content/dropzone/2018/02/black-panther-510x340.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
